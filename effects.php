da3666325a7605823ea6a9131ff2c2480d980873
traviscole <travis@traviscole.me>
2014-02-11 13:22:01 -0500

    Create effects.php
    This file is linked by clicking any of the "Transitions" within default.html

---
+++ effects.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
+	<title>jQuery M0obile Demo</title>   
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />  
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+</head>
+<body>
+<div data-role="page" data-theme="b">
+	<div data-role="header" data-theme="b">
+		<h1>Quikshop</h1>
+	</div>
+	<div data-role="content"> <h1>The  effect</h1><p>Using any of these transition effects couldn't be any easier, simply add <code style="background:#fff; padding: 2px;"><em>data-transition=""</em></code> to your anchor link and you're all set.</p>		<br /><br />
+		<a href="http://www.quikshop.co" data-role="button" data-theme="a" data-icon="back" data-inline="true">Return to Main Page</a> 
+		<a href="http://cipps.eng.ufl.edu" data-role="button" data-theme="c" data-icon="check" data-inline="true">CIPPS</a> 
+	</div>
+	<div data-role="footer" data-position="fixed">
+		<h1>&copy; QuikShop 2014</h1>
+	</div>
+
+</div>
+</body>
+</html>
