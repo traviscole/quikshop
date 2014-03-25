9de63e8d3730727f29ccdc50e9374280dfabfa64
Travis Cole <travis@traviscole.me>
2014-03-25 11:53:00 -0400

    Needs error checking to see if duplicate account.

--- createAcct.php
+++ createAcct.php
@@ -1,46 +1,45 @@
 <?php
-// Allow for errors
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// 	Allow for errors
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
 
-// Connect to the database NOTE: Hostgator may need another connection route than 'localhost'
-$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
+// 	Connect to the database NOTE: Hostgator may need another connection route than 'localhost'
+	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
 
-// Various Includes
-header("access-control-allow-origin: *");
-header("access-control-allow-methods: GET, POST, OPTIONS");
-header("access-control-allow-credentials: true");
-header("access-control-allow-headers: Content-Type, *");
-header("Content-type: application/json");
+// 	Various Includes
+	header("access-control-allow-origin: *");
+	header("access-control-allow-methods: GET, POST, OPTIONS");
+	header("access-control-allow-credentials: true");
+	header("access-control-allow-headers: Content-Type, *");
+	header("Content-type: application/json");
 
-// Parse the log in form if the user has filled it out and pressed "Log In"
-// Returns true if all these values were passed in
-echo "in";
-if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+// 	Parse the log in form if the user has filled it out and pressed "Log In"
+// 	Returns true if all these values were passed in
+	if (isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["password"])) {
 
 //	Assign the passed entry to variables
-    $fName 	= mysqli_real_escape_string($mysqli,$_POST['fname']);
-    $lName 	= mysqli_real_escape_string($mysqli,$_POST['lname']);
-    $eMail 	= mysqli_real_escape_string($mysqli,$_POST['email']);		// Required to not be NULL
-    $pw 	= mysqli_real_escape_string($mysqli,$_POST['password']);	// Required to not be NULL
-    $passwordHashed = password_hash($pw, PASSWORD_DEFAULT);			// Hash PW before inserting
+    		$fName 		= mysqli_real_escape_string($mysqli,$_POST['fname']);
+    		$lName 		= mysqli_real_escape_string($mysqli,$_POST['lname']);
+    		$eMail 		= mysqli_real_escape_string($mysqli,$_POST['email']);		// Required to not be NULL
+    		$pw 		= mysqli_real_escape_string($mysqli,$_POST['password']);	// Required to not be NULL
+    		$passwordHashed = password_hash($pw, PASSWORD_DEFAULT);			// Hash PW before inserting
     
 //	Build the query    
-	$sql="INSERT INTO Users(email,passHash,fname,lname) VALUES('$eMail','$passwordHashed','$fName','$lName')";
+		$sql="INSERT INTO Users(email,passHash,fname,lname) VALUES('$eMail','$passwordHashed','$fName','$lName')";
 
 //	Post the query, 
-	$result = $mysqli->query($sql) or die( $mysqli->error );
+		$result = $mysqli->query($sql) or die( $mysqli->error );
 		if($result){
 			echo "Data for $fname inserted successfully!";
 		}
 		else{ 
 			echo "An error occurred inserting user info";
 		}
-}
-else {
-	echo "The form was not filled out correctly";
-}
+	}
+	else {
+		echo "The form was not filled out correctly";
+	}
 
-// Close the connection
-$mysqli->close();
+// 	Close the connection
+	$mysqli->close();
 ?>
