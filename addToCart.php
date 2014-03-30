07a9855b655f8c8b5e24a1c3f24190d108571d64
Travis Cole <travis@traviscole.me>
2014-03-30 11:06:26 -0400

    .

--- addToCart.php
+++ addToCart.php
@@ -1,6 +1,6 @@
 <?php
 	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
+//	ini_set('display_errors', '1');
 	header("access-control-allow-origin: *");
 	header("access-control-allow-methods: GET, POST, OPTIONS");
 	header("access-control-allow-credentials: true");
@@ -12,27 +12,40 @@
 	$data = json_decode(file_get_contents('php://input'));
 //	var_dump($data);
 
-    $barcode 		= $data->barcode;
-    $storeId 		= $data->undefined;		// can't figure out how to title this field so defaults all around
-    $quantity 		= $data->quantity;
+	if($data) {
+   	 	$barcode 		= $data->barcode;
+    	$storeId 		= $data->undefined;		// can't figure out how to title this field so defaults all around
+    	$quantity 		= $data->quantity;
     	
-	$sql="SELECT * FROM AppItems WHERE storeID='$storeId', barcode='$barcode' LIMIT 1";
-	$result = $mysqli->query($sql) or die( $mysqli->error );
-	if($result)
-    {	
-    	$row = mysqli_fetch_assoc($result);
-    	$itemId = $row['itemID'];
-    	$sql="INSERT INTO AppUsers(email,fname,lname,address,city,state,zip,passHash) VALUES('$eMail','$fName','$lName','$address','$city','$state','$zip','$passwordHashed')";
+		$sql="SELECT * FROM AppItems WHERE storeID='$storeId', barcode='$barcode' LIMIT 1";
+		$result = $mysqli->query($sql) or die( $mysqli->error );
+		if($result)
+    	{	
+    		$row = mysqli_fetch_assoc($result);
+    		$itemId = $row['itemID'];
+    		$itemName = $row['name'];
+    		$sql2="INSERT INTO AppUsers(email,fname,lname,address,city,state,zip,passHash) VALUES('$eMail','$fName','$lName','$address','$city','$state','$zip','$passwordHashed')";
     	
-    	$result = $mysqli->query($sql) or die( $mysqli->error );
-    	if($result)
-		{
+    		$result2 = $mysqli->query($sql2) or die( $mysqli->error );
+    		if($result2)
+			{
+				$response_array['itemName'] = '$itemName';
+				$response_array['success'] = 'error'; 
+			}
+			else{
+				$response_array['status'] = 'error'; 
+				$response_array['reason'] = 'ERROR: Query failed'; 
+			}
+    	}
+    	else{
+    		$response_array['status'] = 'error'; 
+			$response_array['reason'] = 'ERROR: Item Does Not Exist'; 
 		}
+   	}	
+	else {
+   	 	$response_array['status'] = 'error'; 
+   	 	$response_array['reason'] = 'ERROR: No Data Was Passed'; 
     }
-    else{
-    	$response_array['status'] = 'error'; 
-		$response_array['reason'] = 'ERROR: Item Does Not Exist'; 
-    }	
 	
 	echo json_encode($response_array);
 	$mysqli->close();
