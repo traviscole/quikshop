a76fdd8126015fa52a8d1c9228234bbe0e8784e2
Travis Cole <travis@traviscole.me>
2014-03-30 09:22:26 -0400

    Dynamic Cart IDs

--- loginAuth.php
+++ loginAuth.php
@@ -1,7 +1,7 @@
 <?php
 // Actually show errors to the console
 	error_reporting(E_ALL);
-//	ini_set('display_errors', '1');
+	ini_set('display_errors', '1');
 
 /* General includes. Basically limit the actions this file can perform
 to further protect your database from injections and hacks
@@ -15,8 +15,8 @@ Standard web stuff though aparently. It works, i'm leaving it
 	
 	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop"); // Credentials to connnect to the DB
 
-	$data = json_decode(file_get_contents('php://input'));	// Accept JSON file input, decode it to a variable. Save this as data
-//	$data = json_decode(file_get_contents('http://www.quikshop.co/App/loginAuthTest.json'));
+//	$data = json_decode(file_get_contents('php://input'));	// Accept JSON file input, decode it to a variable. Save this as data
+	$data = json_decode(file_get_contents('http://www.quikshop.co/App/loginAuthTest.json'));
 
 //	var_dump($data);	// var_dump is for debugging, prints all PHP variables to the console
 	if($data) {
@@ -37,13 +37,28 @@ Standard web stuff though aparently. It works, i'm leaving it
     		$row = mysqli_fetch_assoc($result);	// Gather the data (row in DB) that was found when the DB was queried
     		$hashedPW 		= $row['passHash'];		// Get the value listed in the passHash field, save to a local PHP variable: hashedPW
     		$userIdResponse = $row['userID'];	// Get the value listed in the userId field, save to local PHP variable: userIdResponse
-//    		$cartIdResponse = $row['cartID'];  	// Get the value listed in the cartId field, save to local PHP variable: cartId 
     
     		/*Response Array is what is returned. It is built dynamically and does not need to 
     			be initialized to an inital size. These simply add a slot and assign a value */
     		$response_array['userId'] = $userIdResponse;
-    		$response_array['email'] = $username;	
-//    		$response_array['cartId'] = $cartIdResponse;
+    		$response_array['email'] = $username;
+    		
+    		$check = $mysqli->query("SELECT * FROM AppLogins WHERE userId='$userIdResponse';");
+			if (mysqli_num_rows($check) == 0) {
+				$sql="INSERT INTO AppUsers(userID) VALUES('$userIdResponse')";
+    			$result = $mysqli->query($sql) or die( $mysqli->error );
+    			$cartIdResponse = $mysqli->insert_id;
+    			$response_array['cartId'] = $cartIdResponse;
+    		if($result)
+    		{
+    		    $response_array['status'] 	= 'success';
+			}
+			else
+			{ 
+				$response_array['status'] = 'error'; 
+				$response_array['reason'] = 'ERROR: Query Was Not Successfully Processed'; 
+			}
+			}
     	} 
     	else 
     	{ 
