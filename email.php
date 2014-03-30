3dc461420849ae043fda5bef2936c21485a29575
abelalvarez89 <abelalvarez89@hotmail.com>
2014-03-30 13:37:08 -0400

    Update email.php
    It is already working I created an email quikshopproject@yahoo.com I couldn't get just quikshop it was already taken.

--- email.php
+++ email.php
@@ -1,93 +1,45 @@
-#!/usr/local/bin/php
 <?php
+	// If we know the userID then we need to look at the loging table and find storeID and cartID so we can search the cart and create a receipt 
+	/*
+	$userID = 
+	
+	$total = 
+	
+	
+*/
+	$email_to = "abelalvarez89@hotmail.com";
  
+    $email_from = "quikshopproject@yahoo.com";
  
-   
- 	//do query and obtain the following infomation
-     
- 
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
- 
-    $items = $_POST['items']; // required
- 
-    $telephone = $_POST['telephone']; // not required
- 
-    $comments = $_POST['comments']; // required
-  
-    $email_to = $_POST['email'];
- 
-    $email_subject = "Items List";
-
+    $email_subject = "Thank you for your purchase";
+	
  
     $error_message = "";
  
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($comments) < 2) {
- 
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
+
+    $email_message = "Cart.\n\nItems-------Quantity---Price\n\n";
  
      
  
     function clean_string($string) {
  
-      $bad = array("content-type","bcc:","to:","cc:","href");
+      $bad = array("content-type","bcc:","to:");
  
       return str_replace($bad,"",$string);
  
     }
- 
-     // here is to prepare what the user will see
+ 	$item = "Coca-Cola";
+	$quantity = 24;
+	$price = 1.29;
+     $i = 0;
+ 	while($i < 3){
+    $email_message .= "$item---$quantity---------$price\n";
+	 
+	$i = $i+1;
+	}
 
-	//produce query and attached the list of items purchased and price
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
- 
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-     
- 
-     
- 
-// create email headers
+	$email_message .= "\n\n\nWe appreciate your business please come back soon\nhttp://www.quikshop.co/";
  
 $headers = 'From: '.$email_from."\r\n".
  
@@ -96,8 +48,6 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
  
 @mail($email_to, $email_subject, $email_message, $headers);  
-
- 
 
  
 ?>
