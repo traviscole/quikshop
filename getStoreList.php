50dd976c5f0faad725ba910f9e18e3a0e695deb4
Travis Cole <travis@traviscole.me>
2014-03-30 00:27:42 -0400

    working

--- getStoreList.php
+++ getStoreList.php
@@ -16,7 +16,7 @@ Standard web stuff though aparently. It works, i'm leaving it
 	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop"); // Credentials to connnect to the DB
 
 // This is the actual SQL query, read it left to right. Only return 1 row. Save as "sql"
-	$sql = "SELECT * FROM AppStores";
+	$sql = "SELECT name,address FROM AppStores";
 
 // Run the query "SQL" against the database. Save as result, some error handling
 	$result = $mysqli->query($sql) or die( $mysqli->error );
@@ -24,24 +24,15 @@ Standard web stuff though aparently. It works, i'm leaving it
 // If there was a response, perform more actions, else return "error"
     if($result)
     {
-    	while($row = mysql_fetch_assoc($result))
-		{
-   			$response_array['status'] = 'success'; 
-   			$storeIdResponse = $row['storeID'];
-   				$response_array['storeId'] = $storeIdResponse; 
-   			$storeNameResponse = $row['name'];
-   				$response_array['name'] = $storeNameResponse; 
+    	$jsonData = array();
+		while ($array = mysqli_fetch_row($result)) {
+    		$jsonData[] = $array;
 		}
+		echo json_encode($jsonData);
     } 
-    else 
-    { 
-    	$response_array['status'] = 'error'; 
-    	$response_array['reason'] = 'ERROR: Query Was Unsuccessful'; 
-    }
 
 /* This takes the response array, converts it to a JSON type and returns it to the caller
 	May need some work within the app, can't tell just yet. I know that HTML gets it no
 	problem, automatically */
-echo json_encode($response_array);
 $mysqli->close();	// Close the connection
 ?>
