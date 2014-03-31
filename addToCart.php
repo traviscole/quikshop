2d342728a9369d91e8aa55ed3eb23242cb0148c0
Travis Cole <travis@traviscole.me>
2014-03-31 07:43:11 -0400

    Error handling for item not in database

--- addToCart.php
+++ addToCart.php
@@ -9,8 +9,8 @@
 	
 	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
 
-	$data = json_decode(file_get_contents('php://input'));
-//	$data = json_decode(file_get_contents('http://www.quikshop.co/App/addToCartTest.json'));
+//	$data = json_decode(file_get_contents('php://input'));
+	$data = json_decode(file_get_contents('http://www.quikshop.co/App/addToCartTest.json'));
 
 	if($data) {
    	 	$barcode 		= $data->barcode;
@@ -26,61 +26,67 @@
     		$itemId 	= $row['itemID'];
     		$itemName 	= $row['name'];
     		
-    		$check = $mysqli->query("SELECT itemID, cartID FROM AppCarts WHERE itemID = '$itemId' AND cartID = '$cartId';");
-			if (mysqli_num_rows($check) == 0) {
-				$sql2="INSERT INTO AppCarts(cartID,itemID,quantity) VALUES('$cartId','$itemId','$quantity')";
+    		if($itemName != null){
+    		  	$check = $mysqli->query("SELECT itemID, cartID FROM AppCarts WHERE itemID = '$itemId' AND cartID = '$cartId';");
+				if (mysqli_num_rows($check) == 0) {
+					$sql2="INSERT INTO AppCarts(cartID,itemID,quantity) VALUES('$cartId','$itemId','$quantity')";
     	
-    			$result2 = $mysqli->query($sql2) or die( $mysqli->error );
-    			if($result2)
-    			{
-    			    $response_array['status'] 	= 'success';
-    			    $response_array['itemName']	= $itemName;
-    			    $response_array['quantityReturned'] = $quantity;
-				}
-				else
-				{ 
-					$response_array['status'] = 'error'; 
-					$response_array['reason'] = 'ERROR: Inserting item failed';
-				}
-			}
-			else {
-				$sql2="SELECT * FROM AppCarts WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
-				$result2 = $mysqli->query($sql2) or die( $mysqli->error );
-				if($result)
-    			{	
-    				$row3 = mysqli_fetch_assoc($result2);
-    				$quantityFetched = $row3['quantity'];
-    				$quantityReturn = $quantityFetched + $quantity;
-    				$sql3="UPDATE AppCarts SET quantity='$quantityReturn' WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
-	
-					$result3 = $mysqli->query($sql3) or die( $mysqli->error );
-					if($result3)
-					{
-						$response_array['status'] = 'success';
-						$response_array['quantityReturned'] = $quantityReturn;
-						$response_array['itemName']	= $itemName;
-					} 
-					else
-					{
-						$response_array['status'] = 'error';
+    				$result2 = $mysqli->query($sql2) or die( $mysqli->error );
+    				if($result2)
+    				{
+    			 	   $response_array['status'] 	= 'success';
+    			 	   $response_array['itemName']	= $itemName;
+    			 	   $response_array['quantityReturned'] = $quantity;
 					}
-    			}
-    			else{
-    				$response_array['status'] = 'error'; 
-					$response_array['reason'] = 'ERROR: Did not successfully update quantity';
-				} 
+					else
+					{ 
+						$response_array['status'] = 'error'; 
+						$response_array['reason'] = 'ERROR: Inserting item failed';
+					}
+				}
+				else {
+					$sql2="SELECT * FROM AppCarts WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
+					$result2 = $mysqli->query($sql2) or die( $mysqli->error );
+					if($result)
+    				{	
+    					$row3 = mysqli_fetch_assoc($result2);
+    					$quantityFetched = $row3['quantity'];
+    					$quantityReturn = $quantityFetched + $quantity;
+    					$sql3="UPDATE AppCarts SET quantity='$quantityReturn' WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
+	
+						$result3 = $mysqli->query($sql3) or die( $mysqli->error );
+						if($result3)
+						{
+							$response_array['status'] = 'success';
+							$response_array['quantityReturned'] = $quantityReturn;
+							$response_array['itemName']	= $itemName;
+						} 
+						else
+						{
+							$response_array['status'] = 'error';
+						}
+    				}
+    				else{
+    					$response_array['status'] = 'error'; 
+						$response_array['reason'] = 'ERROR: Did not successfully update quantity';
+					} 
+				}
+    		}
+    		else{
+    			$response_array['status'] = 'error'; 
+				$response_array['reason'] = 'ERROR: That Item Does Not Exist In the Database'; 
 			}
-    	}
-    	else{
-    		$response_array['status'] = 'error'; 
-			$response_array['reason'] = 'ERROR: Query failed'; 
+   		}
+   		else{
+    			$response_array['status'] = 'error'; 
+				$response_array['reason'] = 'ERROR: Query Failed'; 
 		}
-   	}	
+	}	
 	else {
    	 	$response_array['status'] = 'error'; 
    	 	$response_array['reason'] = 'ERROR: No Data Was Passed'; 
     }
 	
-	echo json_encode($response_array);
-	$mysqli->close();
+echo json_encode($response_array);
+$mysqli->close();
 ?>
