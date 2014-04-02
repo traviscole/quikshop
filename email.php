02a879fd61d6b8f5c4244dd271f8dc8a0a6e8663
abelalvarez89 <abelalvarez89@hotmail.com>
2014-04-02 12:53:18 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,13 +1,34 @@
 <?php
 	// If we know the userID then we need to look at the loging table and find storeID and cartID so we can search the cart and create a receipt 
 	/*
-	$userID = 
+	$userID = $_SESSION('userID');
 	
-	$total = 
+	$storeID = $SESSION('storeID');
+	
+	$total = $_SESSION('total')
+	
+	
 	
 	
 */
-	$email_to = "abelalvarez89@hotmail.com";
+	$storeID = 1;
+	$userID = 7;
+	error_reporting(E_ALL);
+
+	ini_set('display_errors', '1');
+	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
+	
+	$sql  = "Select firstName, lastName, email from Users WHERE userID = $userID";
+	$result = $mysqli->query($sql) or die( $mysqli->error );
+	$row = mysqli_fetch_row($result);
+	
+	$firstName = $row[0];
+	
+	$lastName = $row[1];
+	
+	$email_to = $row[2];
+ 
+ 	print ("Hello $firstName $lastName\n");
  
     $email_from = "quikshopproject@yahoo.com";
  
@@ -17,8 +38,10 @@
     $error_message = "";
  
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+	
+	 $email_message = "Thank you for your purchase $firstName $lastName";
 
-    $email_message = "Cart.\n\nItems-------Quantity---Price\n\n";
+    $email_message .= "\n\n\nCart\n\nItems\t\t\tQuantity\t\tPrice\n\n";
  
      
  
@@ -33,8 +56,12 @@
 	$quantity = 24;
 	$price = 1.29;
      $i = 0;
- 	while($i < 3){
-    $email_message .= "$item---$quantity---------$price\n";
+	
+	$sql  = "Select name, storeID, price from Items WHERE storeID = 1";
+	$result = $mysqli->query($sql) or die( $mysqli->error );
+
+ 	while($row = mysqli_fetch_row($result)){
+    $email_message .= "$row[0]\t\t\t$row[1]\t\t$row[2]\n";
 	 
 	$i = $i+1;
 	}
@@ -49,5 +76,6 @@ $headers = 'From: '.$email_from."\r\n".
  
 @mail($email_to, $email_subject, $email_message, $headers);  
 
+$mysqli->close();
  
 ?>
