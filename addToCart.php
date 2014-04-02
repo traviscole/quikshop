00a8eea8e484429657e8dfcdde01f7b7f69052f0
Nicole Shiver <nicshiver@gmail.com>
2014-04-02 12:02:52 -0400

    Done. Testing now

--- addToCart.php
+++ addToCart.php
@@ -18,7 +18,7 @@
     	$quantity 		= $data->quantity;
     	$storeId 		= $data->storeID;
     	
-		$sql="SELECT * FROM AppItems WHERE storeID='$storeId' AND barcode='$barcode' LIMIT 1";
+		$sql="SELECT * FROM Items WHERE storeID='$storeId' AND barcode='$barcode' LIMIT 1";
 		$result = $mysqli->query($sql) or die( $mysqli->error );
 		if($result)
     	{	
@@ -27,9 +27,9 @@
     		$itemName 	= $row['name'];
     		
     		if($itemName != null){
-    		  	$check = $mysqli->query("SELECT itemID, cartID FROM AppCarts WHERE itemID = '$itemId' AND cartID = '$cartId';");
+    		  	$check = $mysqli->query("SELECT itemID, cartID FROM Carts WHERE itemID = '$itemId' AND cartID = '$cartId';");
 				if (mysqli_num_rows($check) == 0) {
-					$sql2="INSERT INTO AppCarts(cartID,itemID,quantity) VALUES('$cartId','$itemId','$quantity')";
+					$sql2="INSERT INTO Carts(cartID,itemID,quantity) VALUES('$cartId','$itemId','$quantity')";
     	
     				$result2 = $mysqli->query($sql2) or die( $mysqli->error );
     				if($result2)
@@ -45,14 +45,14 @@
 					}
 				}
 				else {
-					$sql2="SELECT * FROM AppCarts WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
+					$sql2="SELECT * FROM Carts WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
 					$result2 = $mysqli->query($sql2) or die( $mysqli->error );
 					if($result)
     				{	
     					$row3 = mysqli_fetch_assoc($result2);
     					$quantityFetched = $row3['quantity'];
     					$quantityReturn = $quantityFetched + $quantity;
-    					$sql3="UPDATE AppCarts SET quantity='$quantityReturn' WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
+    					$sql3="UPDATE Carts SET quantity='$quantityReturn' WHERE cartID='$cartId' AND itemID='$itemId' LIMIT 1";
 	
 						$result3 = $mysqli->query($sql3) or die( $mysqli->error );
 						if($result3)
