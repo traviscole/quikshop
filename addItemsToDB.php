1b51a51dbff445338ea8ff9e2714884cac1f4d90
Travis Cole <travis@traviscole.me>
2014-04-02 10:05:52 -0400

    .

--- addItemsToDB.php
+++ addItemsToDB.php
@@ -22,6 +22,7 @@
    	 		$decimal = mt_rand(1,99)/100;
    	 		$tens = mt_rand(0,15); 
    	 		$price = $tens + $decimal;
+   	 		
    	 		$storeID = $i;
    	 		$mysqli->query("INSERT INTO Items(storeID,name,description,price,barcode,brand) VALUES('$storeID','$name','$description','$price','$barcode','$brand')");
 		}
