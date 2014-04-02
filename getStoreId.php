f81e6bfc2ff54d4a9bbc3b6a89f5ab4cdd08aa23
Nicole Shiver <nicshiver@gmail.com>
2014-04-02 12:36:54 -0400

    .

--- getStoreId.php
+++ getStoreId.php
@@ -9,8 +9,8 @@
 	
 	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
 
-	$data = json_decode(file_get_contents('php://input'));
-//	$data = json_decode(file_get_contents('http://www.quikshop.co/App/getStoreIdTest.json'));
+//	$data = json_decode(file_get_contents('php://input'));
+	$data = json_decode(file_get_contents('http://www.quikshop.co/App/getStoreIdTest.json'));
 
 	if($data) {
     	$name 		= $data->name;
