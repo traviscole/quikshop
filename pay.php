c216d72351995364726e16d32af33dedc0dae93b
abelalvarez89 <abelalvarez89@hotmail.com>
2014-04-02 13:12:36 -0400

    Update pay.php

--- pay.php
+++ pay.php
@@ -55,60 +55,92 @@ Submit Payment<p>Sending Email
         </p>
         </center>
         </font>
-         <?php
-	/*	 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
- 
-    function clean_string($string) {
- 
-      $bad = array("Name:","Payment Method:","Items Purchased:","Total:");
- 
-      return str_replace($bad,"",$string);
- 
-    }
+      <?php
+	// If we know the userID then we need to look at the loging table and find storeID and cartID so we can search the cart and create a receipt 
+	/*
+	$userID = $_SESSION('userID');
 	
- 
-     // here is to prepare what the user will see
-
-	//produce query and attached the list of items purchased and price
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($cardNumber)."\n";
- 
- 	$sqlp  = "Select * from cart WHERE cartID = $cartID";
-	$resultp = $mysqli->query($sqlp) or die( $mysqli->error );
-	$email_message .= "Quantiy	Item	Description	Price\n";
+	$storeID = $SESSION('storeID');
 	
-	While($rowp = mysqli_fetch_row($resultp)){
-		$sqlq  = "Select quantity from cart WHERE itemsID = $rowp[0]";
-		$resultq = $mysqli->query($sqlq) or die( $mysqli->error );
-		$rowq = mysqli_fetch_row($resultq)
-		
-   
+	$total = $_SESSION('total')
 	
-	}
-	$email_message .= "Products".clean_string($price)."\n";
-    $email_message .= "Purchased Items: ".clean_string($price)."\n";
- 
-     
- 
-     $email_to ="abelalvarez89@ufl.edu";
-	 $email_from="abelalvarez89@hotmail.com
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
-
- 
+	
+	
+	
 */
+		$storeID = 1;
+		$userID = 7;
+		$cartID = 3;
+		$total = 13.45;
+		error_reporting(E_ALL);
+		
+		ini_set('display_errors', '1');
+		$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
+		
+		$sql  = "Select firstName, lastName, email from Users WHERE userID = $userID";
+		$result = $mysqli->query($sql) or die( $mysqli->error );
+		$row = mysqli_fetch_row($result);
+		
+		$firstName = $row[0];
+		
+		$lastName = $row[1];
+		
+		$email_to = $row[2];
+		
+		print ("Hello $firstName $lastName\n");
+		
+		$email_from = "quikshopproject@yahoo.com";
+		
+		$email_subject = "Thank you for your purchase";
+		
+		
+		$error_message = "";
+		
+		$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+		
+		$email_message = "Thank you for your purchase $firstName $lastName";
+		
+		$email_message .= "\n\n\nCart\n\nItems\t\t\tQuantity\t\tPrice\n\n";
+		
+		
+		
+		function clean_string($string) {
+		
+			$bad = array("content-type","bcc:","to:");
+		
+			return str_replace($bad,"",$string);
+		
+		}
+		$item = "Coca-Cola";
+		$quantity = 24;
+		$price = 1.29;
+		$i = 0;
+		
+		$sqlinit  = "Select itemID, quantity from Carts WHERE cartID = $cartID";
+		$resultinit = $mysqli->query($sqlinit) or die( $mysqli->error );
+		
+		while($rowinit = mysqli_fetch_row($resultinit)){
+			$sql  = "Select name, price from Items WHERE itemID = $rowinit[0]";
+			$result = $mysqli->query($sql) or die( $mysqli->error );
+			$row = mysqli_fetch_row($result);
+			print("$row[0]\t\t\t$rowinit[1]\t\t$row[1]\n");
+			
+			$email_message .= "$row[0]\t\t\t$rowinit[1]\t\t$$row[1]\n";
+			
+			$i = $i+1;
+		}
+		
+		$email_message .= "\n\t\t\t\t\tTotal: $$total\n\n\nWe appreciate your business please come back soon\nhttp://www.quikshop.co/";
+		
+		$headers = 'From: '.$email_from."\r\n".
+		
+		'Reply-To: '.$email_from."\r\n" .
+		
+		'X-Mailer: PHP/' . phpversion();
+		
+		@mail($email_to, $email_subject, $email_message, $headers);  
+
+
 		
 	}
 	else if(isset($_POST['delete'])){
