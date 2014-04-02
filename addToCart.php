1fd1009fb9ca1096e7ab50d186c38873faa8120c
Nicole Shiver <nicshiver@gmail.com>
2014-04-02 12:26:41 -0400

    .

--- addToCart.php
+++ addToCart.php
@@ -9,8 +9,8 @@
 	
 	$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
 
-//	$data = json_decode(file_get_contents('php://input'));
-	$data = json_decode(file_get_contents('http://www.quikshop.co/App/addToCartTest.json'));
+	$data = json_decode(file_get_contents('php://input'));
+//	$data = json_decode(file_get_contents('http://www.quikshop.co/App/addToCartTest.json'));
 
 	if($data) {
    	 	$barcode 		= $data->barcode;
