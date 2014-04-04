73b07e654a885cd6d86afdd9e733596ae1854198
abelalvarez89 <abelalvarez89@hotmail.com>
2014-04-04 10:56:13 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
 
-<html>
-
+	<title>Quikshop Mobile</title>   
+	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />  
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+  
+    <script>
+		localStorage.setItem("userId", "signOut");
+	</script>
 </head>
+
+<body>
+
+<div data-role="page" data-theme="b">
+
+	<div data-role="header" data-theme="b">
+		<h1><img src="logo.png" width="126" height="26" align="middle" /></h1>
+        	<a href="help.html" data-icon="info" class="ui-btn-right" position:absolute top:50% data-transition="slide">Help</a>
+	</div>
+    
+	<div data-role="content">  
+    	<div id="landmark-1" data-landmark-id="1">
+            	<center> You have been logged out. </center>
+            	<center><a href="https://quikshop.co/default.html" data-role="button">Return To Login</a></center>
+	</div> 
+        </div>
 
 <?php
 			error_reporting(E_ALL);
@@ -24,12 +49,25 @@
 			
 			
 ?>
+	<div data-role="footer" data-position="fixed"  data-theme="b" data--tap-toggle="false">
+		<h1>&copy; Quikshop 2014</h1>
+	</div>
+
+</div>
 
 
-<body>
 </body>
-<?php header('Location: https://quikshop.co/logout.html');
+<?php 
 $mysqli->close();
 ?>
-
 </html>
+
+
+
+
+
+
+
+
+
+
