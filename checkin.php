2e4e3d648a570546110b3aababfc7abc06fd42be
Jose Prado <josegpradoa@gmail.com>
2014-04-08 22:52:44 -0400

    Update checkin.php

--- checkin.php
+++ checkin.php
@@ -1,69 +1,89 @@
-<?php 
+<?php
 //Jose Prado
 
 sleep(2);
 //Sanitize incoming data and store in variable
-$zipcode = trim(stripslashes(htmlspecialchars($_POST['zipcode'])));	
+$zipcode = trim(stripslashes(htmlspecialchars($_POST['zipcode'])));
 $long = trim(stripslashes(htmlspecialchars($_POST['long'])));
-$lat = trim(stripslashes(htmlspecialchars($_POST['lat'])));   
+$lat = trim(stripslashes(htmlspecialchars($_POST['lat'])));
 $humancheck = $_POST['humancheck'];
 $honeypot = $_POST['honeypot'];
 
-if ($honeypot == 'http://' && empty($humancheck)) {	
-		
-		//Validate data and return success or error message
-		$error_message = '';	
+if ($honeypot == 'http://' && empty($humancheck)) {
+
+    //Validate data and return success or error message
+    $error_message = '';
 
 
-		if (empty($zipcode) && empty($long)) {
-				    
-					$error_message .= "<p>A valid zipcode is required .</p>";			   
-		}
-		if (!empty($error_message)) {
-					$return['error'] = true;
-					$return['msg'] = "<h3>Oops! The request was successful but your form is not filled out correctly.</h3>".$error_message;					
-					echo json_encode($return);
-					exit();
-		} else {
+    if (empty($zipcode) && empty($long)) {
+
+        $error_message .= "<p>A valid zipcode is required .</p>";
+    }
+    if (!empty($error_message)) {
+        $return['error'] = true;
+        $return['msg'] = "<h3>Oops! The request was successful but your form is not filled out correctly.</h3>".$error_message;
+        echo json_encode($return);
+        exit();
+    } else {
 
 
-			//Connect to the database NOTE: Hostgator may need another connection route than 'localhost'
-//			$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
+        //Connect to the database NOTE: Hostgator may need another connection route than 'localhost'
+        $mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
 
-			//check if we made a connection
-//			if ($mysqli->connect_errno) {
-  // 				$return['error'] = true;
-	//			$return['msg'] = "<h3>Oops! We could not connect to the server.</h3>".$error_message;					
-	//			echo json_encode($return);
+        //check if we made a connection
+        if ($mysqli->connect_errno) {
+            $return['error'] = true;
+            $return['msg'] = "<h3>Oops! We could not connect to the server.</h3>".$error_message;
+            echo json_encode($return);
 
-    	//			exit();
-	//		}
+            exit();
+        }
 
-	//		$hashPassword = password_hash($password, PASSWORD_DEFAULT);
-							  
+		//deciding which one to use depending on the user input
+        if (!empty($zipcode))
+            $result = $mysqli->query("SELECT name from Stores where zip = $zipcode");
+        else
+            $result = $mysqli->query("SELECT name, address from Stores where zip = 32608");
 
-	//		if($mysqli->query("SELECT name, address from Stores where zip = $zipcode") == False){
-	//			$return['error'] = true;
-	//			$return['msg'] = "<h3>Oops! We could not do the query.</h3>".$error_message;					
-	//			echo json_encode($return);
+		//throw an error if the query didn't work
+        if ($result == False) {
+            $return['error'] = true;
+            $return['msg'] = "<h3>Oops! We could not do the query.</h3>".$error_message;
+            echo json_encode($return);
 
-    	//			exit();
-					
-	//		}
+            exit();
 
-	//		//Close the connection
-	//		$mysqli->close();
+        }
 
-			$return['error'] = false;
-			$return['msg'] = "<p>Select one of the stores </p>"; 
-			echo json_encode($return);
-		  }		
+
+        $return = array();
+
+        while ($t = mysqli_fetch_assoc($result)) {
+            $return['stores'][] = $t['name'];
+        }
+
+
+
+
+
+        //Close the connection
+        $mysqli->close();
+
+        $return['error'] = false;
+        $return['msg'] = "<p>Select one of the stores </p>";
+
+        //print json_encode($rows);
+
+        echo json_encode($return);
+    }
 } else {
-	
-	$return['error'] = true;
-	$return['msg'] = "<h3>Oops! There was a problem with your submission. Please try again.</h3>";	
-	echo json_encode($return);
+
+    $return['error'] = true;
+    $return['msg'] = "<h3>Oops! There was a problem with your submission. Please try again.</h3>";
+    echo json_encode($return);
 }
-	
-?> 
+
+?>
+
+
 
