19dbc66c42310a3c4a275c356fd9e73664c76da6
Jose Prado <josegpradoa@gmail.com>
2014-04-08 20:35:22 -0400

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -252,4 +252,10 @@
 
 		};
 
+function openWin()
+{
+var myWindow = window.open("","MsgWindow","width=200,height=100");
+myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
+}
+
 
