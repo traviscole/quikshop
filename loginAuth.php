782b4dc1af110853cea12e24cab1008bc2fd441d
sky-existence <jclannon94@yahoo.com>
2014-04-09 08:59:55 -0400

    Update loginAuth.php

--- loginAuth.php
+++ loginAuth.php
@@ -20,37 +20,76 @@
 	if (isset($_POST["username"]) && isset($_POST["password"])) {
 
 //	Assign the passed entry to variables
-    		$username = mysqli_real_escape_string($mysqli,$_POST['username']);
-    		$password = mysqli_real_escape_string($mysqli,$_POST['password']);
-    	
-//	Assign the username to the response array, for saving to local storage
-//	back in default.html
-		$response_array['userId'] = $username;
+    		$username = $data->username;
+		$password = $data->password;
 
-//	Build the SQL call
-    		$sql = "SELECT userId, email, password FROM Users WHERE email='$username' LIMIT 1";
-//	Call the database, save the result in the variable RESULT
-    		$result = $mysqli->query($sql) or die( $mysqli->error );
-//	Extract the row data of the result. Save as ROW
-    		$row = mysqli_fetch_assoc($result);
-//	Extraxt the hashed password out of the database
-    		$hashDB = $row['password'];
+<<<<<<< HEAD
+// This is the actual SQL query, read it left to right. Only return 1 row. Save as "sql"
+$sql = "SELECT * FROM Users WHERE email='$username' LIMIT 1";
+=======
+// Build the SQL call
+     $sql = "SELECT userId, email, password FROM Users WHERE email='$username' LIMIT 1";
+// Call the database, save the result in the variable RESULT
+     $result = $mysqli->query($sql) or die( $mysqli->error );
+// Extract the row data of the result. Save as ROQ
+     $row = mysqli_fetch_assoc($result);
+// Extraxt the hashed password out of the database
+     $hashDB = $row['password'];
+>>>>>>> master
 
-//	See if the password matches
-		if (password_verify($password, $hashDB)) {
-
-		//storing the global attributes
-		$_SESSION['userID'] = $row['userId'];
-
-
-//		If so, write success to the response aray
-    			$response_array['status'] = 'success'; 
-    		} 
+// Run the query "SQL" against the database. Save as result, some error handling
+    $result = $mysqli->query($sql) or die( $mysqli->error );
     
-    		else {
-//		If not, write error to response
-          		$response_array['status'] = 'error'; 
-    		}
+// If there was a response, perform more actions, else return "error"
+     if($result)
+     {
+     $row = mysqli_fetch_assoc($result);	// Gather the data (row in DB) that was found when the DB was queried
+     $hashedPW = $row['password'];	// Get the value listed in the passHash field, save to a local PHP variable: hashedPW
+     $userIdResponse = $row['userID'];	// Get the value listed in the userId field, save to local PHP variable: userIdResponse
+    
+     /*Response Array is what is returned. It is built dynamically and does not need to
+be initialized to an inital size. These simply add a slot and assign a value */
+     $response_array['userID'] = $userIdResponse;
+     $response_array['email'] = $username;
+    
+     $check = $mysqli->query("SELECT * FROM Logins WHERE userID='$userIdResponse';");
+if (mysqli_num_rows($check) == 0) {
+$sql="INSERT INTO Logins(userID) VALUES('$userIdResponse')";
+     $result = $mysqli->query($sql) or die( $mysqli->error );
+     if($result)
+     {
+     $result3 = $mysqli->query("SELECT cartID FROM Logins WHERE userID='$userIdResponse';");
+     $row3 = mysqli_fetch_assoc($result3);
+     $response_array['cartID'] = $row3['cartID'];
+     }
+}
+else {
+     $result2 = $mysqli->query("SELECT cartID FROM Logins WHERE userID='$userIdResponse';");
+     $row2 = mysqli_fetch_assoc($result2);
+     $response_array['cartID'] = $row2['cartID'];
+     }
+     }
+     else
+     {
+     $response_array['status'] = 'error';
+     $response_array['reason'] = 'ERROR: Query Was Unsuccessful';
+     }
+    
+    /* PHP function to see if "password" (from the calling program) matches the value
+we have in the database. We use a PHP function because we store the hashed value
+the same sting can have basically unlimited hashes so the function handles matching
+we never see the plaintext value (unless we drop variables to console which we
+shouldn't once these are working */
+    
+     if (password_verify($password, $hashedPW))
+     {
+     $response_array['status'] = 'success'; // The program uses the value of "status" to know to move to logged in state or not
+     }
+     else
+     {
+     $response_array['status'] = 'error';
+     $response_array['reason'] = 'ERROR: Password was Incorrect';
+     }
     
 // 	Encode the response in JSON, it is automatically passed back to the caller. Also echo this to the console
 		echo json_encode($response_array);
