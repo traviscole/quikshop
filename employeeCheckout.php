083ebc1f937c9d8eb766b1b11673fadbff6b9ae7
abelalvarez89 <abelalvarez89@hotmail.com>
2014-04-14 11:16:46 -0400

    Create employeeCheckout.php

---
+++ employeeCheckout.php
@@ -0,0 +1,125 @@
+<?php 
+   session_start(); 
+  ?>
+  
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+  		
+  			// Allow for errors
+  			error_reporting(E_ALL);
+  			ini_set('display_errors', '1');
+  
+  			// Connect to the database NOTE: Hostgator may need another connection route than 'localhost'
+  			$mysqli = new mysqli("quikshop.co","cx300_cen3031","[cEn..3031!]","cx300_quikshop");
+  
+  			// Various Includes --getting the userId and cartId from the current logged in user
+  
+  			$userID = $_SESSION['customerID'];
+  			
+			
+  			print ("UserID:	$userID");
+			
+			
+			$sqlCart  = "select cartID from Logins where userID = $userID";
+  			$resultCart = $mysqli->query($sqlCart) or die( $mysqli->error );
+			$rowCart = mysqli_fetch_row($resultCart);
+  			$cartID = $rowCart[0];
+  			
+			
+  			$sql  = "SELECT itemID, quantity From Carts where cartID = $cartID";
+  
+  			//	Call the database, save the result in the variable RESULT
+  			$result = $mysqli->query($sql) or die( $mysqli->error );
+  
+  			//	Extract the row data of the result. Save as ROQ
+  
+  
+  			$sum = 0;
+  			$quantity = 0;
+  			//while loop will look in the customer's cart all the items	
+		
+  			while($row = mysqli_fetch_row($result)){
+  			//save the quantity of the current item
+  			$itemID = $row[0];
+  			$quantity = $row[1];
+			
+  			//look for the items price using 
+  			$sqlr  = "SELECT price From Items where itemID = $itemID ";
+  			$resultr = $mysqli->query($sqlr) or die( $mysqli->error );
+  			$rowr = mysqli_fetch_row($resultr);
+  
+  			$sum = $sum + $quantity*$rowr[0];
+  				
+  			}
+			
+  			?>
+<br />
+<center>
+  
+  
+  <font size="18">Amount Due 
+	  <?php
+  					print("$$sum");
+  				?>
+  </font>
+  </center>
+ 
+ 			<?php
+   				$sqlcard  = "SELECT Type,ID From CreditCards where userID = $userID ";
+  				$resultcard = $mysqli->query($sqlcard) or die( $mysqli->error );
+						
+  					
+  			?>
+  		<form id ="payNow" method="post" action="employeePay.php">
+  		<div data-role="fieldcontain" class="ui-hide-label">  
+  			<div style="text-align:center">  
+
+  				<select name="Cards">
+  					<?php
+  														
+  						print($rowcard[0]);
+  						$x = 0;
+  							$cardValue = 1;
+  						while($rowcard = mysqli_fetch_row($resultcard)){
+  							
+  					?>
+                 
+  					<option  value=<?php echo("$rowcard[1]");?>><?php echo($rowcard[0]);?></option>
+  					<?php
+  						$x = $x+1;
+						}
+  					?>
+                      
+  				</select>
+					<input type="hidden" name="total" value= <?php echo $sum;?>>
+  					<input type="hidden" name="userId" value= <?php echo $userID;?>>
+                	<input type="hidden" name="cartId" value= <?php echo $cartID;?>>
+  				<br />
+                  <br />
+  				
+  				<input type="submit" name="submit" value="Pay Now">
+                 	<br />
+  				<br />
+                  <input type="submit"  name="delete" value="Delete Card">
+                  <br />
+  			</div>
+  		</div>
+  		</form>
+  
+  		<form id="addCard" method="post"  action="AddPayment.html">
+  			<div data-role="fieldcontain" class="ui-hide-label">  
+  				<div style="text-align:center">  
+  					<input type="submit"  value="Add Card" >
+  				</div>
+  			</div>
+  		</form>
+</body>
+</html>
